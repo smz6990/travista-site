2ae66275cf0c65c2bddf8552c02cec7c81093460
smz6990 <saleh.mohammadzadeh@gmail.com>
2022-07-15 00:21:05 +0430

    created models for blog and website
    created post class in app blog models.py and created contact class in app website models.py, and run makemigrations and migrate . and do some of admin.py in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    
+    pass
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Post (models.Model):
+    
+    # image = models.ImageField()
+    # author = models.ImageField() it has to be a ForeignKey()
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tags 
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return ' {} - {} '.format(self.id,self.title)
