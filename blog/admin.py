f648379f6b6c812427b6491c3ec5588bd73b53bd
smz6990 <saleh.mohammadzadeh@gmail.com>
2022-07-15 20:27:37 +0430

    changing the blog\views.index_view to show our content in page
    changing the respective template for index_view in blog/views to show content from our database

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    fields = ('title','content','published_date')
+    fields = ('title','content','status','published_date')
     list_display = ('title' ,'counted_views','status','published_date','created_date')
     list_filter = ('status',)
     search_fields = ['title','content']
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
 def index_view(request):
-    return render(request,'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 
 def single_view(request):
     return render(request,'blog/blog-single.html')
