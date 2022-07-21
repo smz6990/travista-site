b94bc17e1041c9031389868bae19217f73062bcd
smz6990 <saleh.mohammadzadeh@gmail.com>
2022-07-21 23:40:36 +0430

    creating comment model and register it in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from blog.models import Post,Category
+from blog.models import Post,Category,Comment
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    # fields = ('author','title','content','status','published_date',)
     list_display = ('title' ,'author','counted_views','status','published_date','created_date')
     list_filter = ('status','author',)
     search_fields = ['title','content']
     summernote_fields = ('content',)
     
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name','post','email','approved','created_date')
+    list_filter = ('post','approved','email','name')
+    search_fields = ['name','post']
+    
+
 admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -14,17 +14,13 @@ class Category(models.Model):
 class Post (models.Model):
     
     image = models.ImageField(upload_to = 'blog/' , default = 'blog/default.jpg')
-    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True) 
     title = models.CharField(max_length=255)
     content = models.TextField()
-    
     tags = TaggableManager() 
-    category = models.ManyToManyField(Category)
-    
+    category = models.ManyToManyField(Category)  
     counted_views = models.IntegerField(default=0)
-    status = models.BooleanField(default=False)
-    
+    status = models.BooleanField(default=False)  
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -37,3 +33,21 @@ class Post (models.Model):
     
     def get_absolute_url(self):
         return reverse('blog:single',kwargs={'id':self.id})
+
+
+class Comment(models.Model):
+    
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_date']
+        
+    def __str__(self):
+        return self.name
